df181c749f4314252b68da34eb7a0537059b6659
Student <student@ECAW01152-PC10.local>
2017-02-21 20:08:17 -0500

    project-01-quiz

---
+++ js/main.js
@@ -0,0 +1,112 @@
+var userInput =  document.getElementById("userInput").value;
+var answers = [10]; 
+new array('userInput');
+document.write('userInput' + 'numberquestion');
+
+var name=('sky-color');
+if (userInput[i]==blue[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0; 
+       var name=("dog-legs");
+if (userInput[i]==four[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+       var name=("mythical-reptile");
+if (userInput[i]==dragon[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+       var name=("left-brain");
+if (userInput[i]==intelligence[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+
+       var name=("right-brain");
+if (userInput[i]==creativity[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+
+       var name ="biggest-planet";
+if (userInput[i]==jupiter[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+       var name=("dinosaur-horns");
+if (userInput[i]==tricaratops[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+
+      var name=("character-pikachu");
+if (userInput[i]==pokemon[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+
+
+      var name=("math-question");
+if (userInput[i]==255[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+
+
+      var name=("one-piece");
+if (userInput[i]==anime[i]) {
+    score += 1;
+    console.log(score);
+   } else{
+       score += 0;
+
+
+
+var score = function(){
+var score = 0;
+var numberquestion=10;
+
+for ( i = 0; i < numberquestion; i++){
+   if (userInput[i]==answers[i]) {
+   	score += 1;
+  	console.log(score);
+   } else{
+   	   score += 0;
+   }
+
+// currently non-functioning
+var returnscore = function(){
+alert("Your score is "+Score()+"/10");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
